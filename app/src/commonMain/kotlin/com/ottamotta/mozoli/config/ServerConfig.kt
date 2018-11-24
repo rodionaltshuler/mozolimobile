2dafdf2b3c7f6728f9f6326cf35167d7eb72f289
Rodion <altshuler.rodion@gmail.com>
2018-11-24 23:09:22 +0200

    Switch to https://api.mozo.li

--- app/src/commonMain/kotlin/com/ottamotta/mozoli/config/ServerConfig.kt
+++ app/src/commonMain/kotlin/com/ottamotta/mozoli/config/ServerConfig.kt
@@ -2,7 +2,7 @@ package com.ottamotta.mozoli.config
 
 object ServerConfig {
 
-    val SERVER_URL = "http://ec2-13-59-171-241.us-east-2.compute.amazonaws.com:8080"
+    val SERVER_URL = "https://api.mozo.li"
 
 
 }
